4a53825a44ab28ccf49cb5a79bb7d27821f10405
Oli Legat <olegat@google.com>
2023-01-26 11:38:36 +0000

    Subtitute the names of some generated examples
    Names like 'string_operator+=' cannot be used a CMake target names. So
    replace these with [_a-z] characters.

--- cplusplus.com.py
+++ cplusplus.com.py
@@ -187,6 +187,21 @@ class CplusplusDotComStringSpider(Spider):
       (a.attrib['href'] for a in page.xpath('//a'))
     ))
 
+def example_name(url):
+  # Get the URL base name ignoring any trailing slashes
+  basename = os.path.basename(os.path.dirname(url+'/'))
+  # Replace characters that are invalid for CMake target names
+  # TODO find & replace stuff here in basename
+  substitutions = {
+    'operator+=' : 'operator_plusequal',
+    'operator+'  : 'operator_plus',
+    'operator='  : 'operator_equal',
+    'operator[]' : 'operator_squarebrackets'
+  }
+  for k,v in substitutions.items():
+    basename = basename.replace(k, v)
+  return 'string_' + basename
+
 def main():
   spider = CplusplusDotComStringSpider(
     starturl = 'http://cplusplus.com/reference/string/string',
@@ -199,12 +214,10 @@ def main():
     assert len(source) in [0,1]
     assert len(output) in [0,1]
     if len(source) == 1 and len(output) == 1:
-      # Get the URL base name ignoring any trailing slashes
-      basename = 'string_' + os.path.basename(os.path.dirname(page.url+'/'))
       lines = [ '[URL]', page.url, '',
                 '[Source]', source[0].text_content(), '',
                 '[Output]', output[0].text_content()]
-      path = os.path.join('out', basename + '.cpp.in')
+      path = os.path.join('out', example_name(page.url) + '.cpp.in')
       with open(path, 'w') as f:
         logging.info(f'writing file: {path}')
         f.write('\n'.join(lines))
