a9df337e2da3b9bd4e9298e561d94df4179c75ff
Oli Legat <olegat@google.com>
2023-01-27 04:17:01 -0800

    Implement test/term_win32.cpp

--- test/term_win32.cpp
+++ test/term_win32.cpp
@@ -5,13 +5,41 @@
 
 #include "term.hpp"
 
-bool term::istty()
+#include <windows.h>
+
+constexpr WORD default_color = 0x07; // white on black
+static    WORD current_color = default_color;
+
+static HANDLE get_console_handle()
 {
-  #pragma message("term::istty() is not implementated")
-  return false;
+  static HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+  return hConsole;
 }
 
-void term::write(const term::style& sty)
+static WORD get_attributes(term::color col, WORD mask)
 {
-  #pragma message("term::write(const term::style&) is not implementated")
+  switch (col) {
+    case term::color::Current:  return current_color & mask;
+    case term::color::Reset:    return default_color & mask;
+    default: break;
+  }
+  WORD attr = static_cast<WORD>(col);
+  return (attr | (attr<<4)) & mask;
+}
+
+static WORD get_attributes(const term::style& styl) {
+  const WORD fgbits = get_attributes( styl.foreground, 0x0F );
+  const WORD bgbits = get_attributes( styl.background, 0xF0 );
+  return fgbits | bgbits;
+}
+
+bool term::istty()
+{
+  return get_console_handle() != INVALID_HANDLE_VALUE;
+}
+
+void term::write(const term::style& styl)
+{
+  SetConsoleTextAttribute( get_console_handle(),
+                           get_attributes( styl ));
 }
